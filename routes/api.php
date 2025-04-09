35bffd4ca44cc227722fa8c81b6aa9cc09bdff81
GUSTAVO <Gustavoandressanchezceron@gmail.com>
2025-04-08 20:47:26 -0500

    a

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UsuarioController;
+use App\Http\Controllers\Api\ProductoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,12 +20,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-use App\Http\Controllers\Api\UsuarioController;
-use App\Http\Controllers\Api\ProductoController;
 
-Route::get('/usuarios', [UsuarioController::class, 'index']);
-Route::get('/usuarios/{id}', [UsuarioController::class, 'show']);
+// Route::get('/usuarios', [UsuarioController::class, 'index']);
+// Route::get('/usuarios/{id}', [UsuarioController::class, 'show']);
 
-Route::post('/usuarios', [UsuarioController::class, 'store']);
-Route::put('/usuarios/{id}', [UsuarioController::class, 'update']);
-Route::delete('/usuarios/{id}', [UsuarioController::class, 'destroy']);
+// Route::post('/usuarios', [UsuarioController::class, 'store']);
+// Route::put('/usuarios/{id}', [UsuarioController::class, 'update']);
+// Route::delete('/usuarios/{id}', [UsuarioController::class, 'destroy']);
